60788d0c3d6b12d4f88c2839570f0868f58d7a42
Matthew Kaufer <mjkaufer@gmail.com>
2015-07-06 15:10:32 -0400

    Basic functionality working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
+var baseNote = teoria.note('C3')
 
-function makeSong(note){
+var chromatics = [
+	teoria.note('e2').scale('chromatic').notes(),
+	teoria.note('c3').scale('chromatic').notes(),
+	teoria.note('g#3').scale('chromatic').notes(),
+]
+
+
+
+var pitchMap = {}
+
+var maxFret = 20;
+
+var strings = ["E", "A", "D", "G"]
+var remaining = []
+var lastNoteString = ""
+
+var noteTime = 5000
+var resultTime = 4000
+
+hideResults()
+
+chromatics = chromatics.reduce(function(prev, curr, index, arr){
+	return prev.concat(curr)
+}, [])
+
+chromatics = chromatics.reduce(function(prev, curr, index, arr){
+	var unique = prev.every(function(el){
+		return el.toString() != curr.toString()
+	})
+	if(unique)
+		prev.push(curr)
+	return prev
+}, [])
+
+chromatics.forEach(function(e, i){
+
+	var stringFrets = makeStringFrets(i)
+
+	pitchMap[e.toString()] = stringFrets
+
+})
+
+startGame()
+
+function makeNote(note){
 
 	return "X:1\n"+
 	"V: 1 clef=bass\n"+
@@ -10,26 +55,10 @@ function makeSong(note){
 
 }
 
-function renderSong(note){
-	ABCJS.renderAbc('sheetOutput', makeSong(note))
+function renderNote(note){
+	ABCJS.renderAbc('sheetOutput', makeNote(note))
 }
 
-var baseNote = teoria.note('C3')
-
-var chromatics = [
-
-	teoria.note('c2').scale('chromatic').notes(),
-	teoria.note('c3').scale('chromatic').notes()
-
-]
-
-chromatics = chromatics.reduce(function(prev, curr, index, arr){
-	return prev.concat(curr)
-}, [])
-
-renderSong("A,,")
-
-var remaining = []
 
 function plotRandomNote(){
 	var teoriaNote = randomNote()
@@ -37,16 +66,35 @@ function plotRandomNote(){
 	makeAbcNote(teoriaNote)
 }
 
-function randomNote(){
+function showNoteInfo(teoriaNote){
+	$('infoOutput').style.display = "none"
+	$('note').textContent = teoriaNote.name().toUpperCase()
+	$('octave').textContent = parseInt(teoriaNote.octave())
+	$('accidental').innerHTML = teoriaNote.accidental()
+
+	$('tab').innerHTML = generateTab(teoriaNote.toString())
+
+}
+
+function randomNote(pop){
+
+	if(pop === undefined)
+		pop = true
 
 	if(remaining.length == 0)
 		remaining = chromatics.slice()
 
-	var index = parseInt(Math.random() * remaining.length)	
+	var index;
+	do{
+		index = parseInt(Math.random() * remaining.length)
+	} while(remaining[index].toString() == lastNoteString)
 	
 	var note = remaining[index]
 
-	remaining.splice(index, 1)
+	lastNoteString = note.toString()
+
+	if(pop)
+		remaining.splice(index, 1)
 
 	return note
 }
@@ -73,12 +121,15 @@ function makeAbcNote(teoriaNote){
 			abcNote += ","
 	}
 
-	renderSong(abcNote)
+	renderNote(abcNote)
 	return abcNote
 
 }
 
-function convertNote(scientificNote){//converts a note with format xn. (a4b, for instance) and turns it into the corresponding, ABC readable note string
+function convertNote(scientificNote, render){//converts a note with format xn. (a4b, for instance) and turns it into the corresponding, ABC readable note string
+
+	if(render === undefined)
+		render = true
 
 	var noteName = scientificNote.replace(/[0-9]/g,"")
 
@@ -89,8 +140,6 @@ function convertNote(scientificNote){//converts a note with format xn. (a4b, for
 	var accidental = noteName.substring(noteName.indexOf(note) + note.length)
 
 	var abcAccidental = ""
-
-	console.log(noteName, octave, note, accidental)
 
 	if(accidental == "b")
 		abcAccidental = "_"
@@ -110,6 +159,86 @@ function convertNote(scientificNote){//converts a note with format xn. (a4b, for
 
 	// startNote = startNote.replace(/',/g, "")//remove redundancies
 	// startNote = startNote.replace(//)
-	renderSong(abcNote)
+	if(render)
+		renderNote(abcNote)
+
 	return abcNote
+}
+
+function $(id){
+	return document.getElementById(id)
+}
+
+
+function makeStringFrets(eFret){//generates a list of similar frets based on the fret of a note on the e string
+
+	var ret = {}
+
+	strings.forEach(function(e, i){
+		var fret = eFret - 5 * i
+		if(fret < 0 || fret > maxFret)
+			return
+		ret[e] = fret
+	})
+
+	return ret
+}
+
+
+function generateTab(teoriaPitch){
+	var length = 4
+	
+	var noteStrings = pitchMap[teoriaPitch]
+
+	var tabLength = strings.length
+
+	var output = ""
+
+	for(var i = 0; i < strings.length; i++){
+		var string = strings[i]
+		var val = "-"
+		if(noteStrings[string] !== undefined){
+			val += noteStrings[string]
+		}
+
+		while(val.length < length)
+			val += "-"
+
+		output = string + " " + val + "\n" + output
+
+	}
+
+	return output.replace(/-/g, "&#8213;").replace(/\n/g, "<br>")
+
+}
+
+
+function hideResults(){
+	$('infoOutput').style.display = "none"
+}
+
+function showResults(teoriaNote){
+	if(teoriaNote)
+		showNoteInfo(teoriaNote)
+	$('infoOutput').style.display = ""
+}
+
+
+function startGame(){
+	gameStep()
+	return  setInterval(function(){
+		gameStep()
+	}, noteTime + resultTime)
+}
+
+function gameStep(){
+	hideResults()
+
+	var note = randomNote()
+
+	convertNote(note.toString())
+
+	setTimeout(function(){
+		showResults(note)
+	}, noteTime)
 }
